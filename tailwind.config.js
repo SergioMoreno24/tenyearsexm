c43b9cb38f142ff1d40cdb648df97abd0271639b
sergio_moreno <sergio.moreno2410@gmail.com>
2023-06-21 20:35:58 -0600

    Added styles and html form structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html", "./js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        blue: {
+          exm:'#00A1C6',
+          exm2: '#123D81',
+          exm3: '#28255A',
+        },
+        purple: {
+          exm: '#8D568E',
+        },
+        yellow: {
+          exm: '#FFDA2A',
+        },
+        green: {
+          exm: '#008A55',
+        },
+      },
+    },
   },
   plugins: [],
 }
